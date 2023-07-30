dcfb2dea5eafd88ef36ab31ad4e1cfe99162039c
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-07-30 16:22:31 +0530

    Menu for mobile view copied to off-canvas

---
+++ script.js
@@ -0,0 +1,18 @@
+// Copy menu for mobile view
+function copyMenu() {
+    // Copy departments
+    let fromDepartments = document.querySelector('.dpt-cat');
+    let toDepartments = document.querySelector('.departments');
+    toDepartments.innerHTML = fromDepartments.innerHTML; 
+
+    // Copy nav menu
+    let fromNav = document.querySelector('.header-nav nav');
+    let toNav = document.querySelector('.off-canvas nav');
+    toNav.innerHTML = fromNav.innerHTML;
+
+    // Copy Top-nav
+    let fromHeaderTop = document.querySelector('.header-top');
+    let toTopNav = document.querySelector('.off-canvas .thetop-nav');
+    toTopNav.innerHTML = fromHeaderTop.innerHTML;
+}
+copyMenu();
