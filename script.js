7f5cb2f3c6433e0c36d84b10f43201b45380cfac
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-08-02 00:01:18 +0530

    feature - Slider added

--- script.js
+++ script.js
@@ -40,3 +40,15 @@ function toggle(e) {
     // if(this.closest('.has-child').classList != 'expand');
     this.closest(".has-child").classList.toggle("expand");
 }
+
+// Slider
+const swiper = new Swiper('.swiper', {
+    
+    loop: true,
+  
+    
+    pagination: {
+      el: '.swiper-pagination',
+    }
+    
+  });
